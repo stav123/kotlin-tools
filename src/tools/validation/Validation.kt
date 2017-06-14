4c67837e55f2e1efa0918370c2bdb24c8fe38e02
stefan.stavrakev <stefan.stavrakev@novarto.com>
2017-06-14 11:36:17 +0300

    added on validation

--- src/tools/validation/Validation.kt
+++ src/tools/validation/Validation.kt
@@ -7,14 +7,41 @@ import java.util.function.Predicate
  */
 
 
+fun main(args: Array<String>) {
+
+
+    println(Validation<String, String>()
+            .rule(Predicate { p -> p.length > 5 }, "Length Should be < than 5", "test")
+            .on("test", "zxc"))
+
+
+}
+
+
 class Validation<T, R> {
 
-    var rules = mutableListOf<Pair<Predicate<T>, R>>()
+    private val rules = mutableListOf<Pair<Predicate<T>, R>>()
 
-    fun rule(predicate: Predicate<T> , result: R, reason: String) : Validation<T,R>{
-            rules.add(Pair(predicate, result))
+    fun rule(p: Predicate<T>, result: R, reason: String): Validation<T, R> {
+        rules.add(Pair(p, result))
         return this
+    }
+
+    fun on(t: T, default: R): R {
+        return rules.filter { it.first.test(t) }.map { pair -> pair.second }.getOrElse(0, { default })
     }
 
 
 }
+
+object Helpers {
+
+    fun <T> accept(t: T, c: (T) -> (Unit)) : Unit = if (t != null) c(t) else Unit
+    fun <T, R> change(t: T, m: (T) -> (R)) = if (t != null) m(t) else Unit
+    fun <T> either(a: T, b: T) = a ?: b
+    fun <T> each(c: Collection<T>, t: (T) -> (Unit)) : Unit = accept(c, {c -> c.forEach(t)})
+
+
+}
+
+data class Vehicle(var a: String, var b: String)
